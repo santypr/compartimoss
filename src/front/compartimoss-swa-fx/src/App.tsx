ea90bde39a1d9870481c09875af647d1014019b7
Santiago Porras <santypr@gmail.com>
2023-06-06 02:25:41 +0100

    fixed app route

--- src/front/compartimoss-swa-fx/src/App.tsx
+++ src/front/compartimoss-swa-fx/src/App.tsx
@@ -1,14 +1,24 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [hello, setHello] = useState('');
+
+  useEffect(() => {
+    (async function () {
+      const { text } = await( await fetch(`/api/hellocompartimoss`)).json();
+      setHello(text);
+    })();
+  });
+
 
   return (
     <>
       <div>
+        <h1>{hello}</h1>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
